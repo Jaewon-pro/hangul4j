a31100fc31900f147ce1f708f99488048a0a714f
Jaewon-pro <jaewonlee.pro@gmail.com>
2023-10-31 00:14:04 +0900

    test(compare): add comparing, contains

---
+++ src/test/java/HangulComparingTest.java
@@ -0,0 +1,40 @@
+import io.github.jaewonpro.HangulUtils;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class HangulComparingTest {
+
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/compare/containing.csv", numLinesToSkip = 1)
+    void givenText_whenCheckContaining_returnResult(
+            final String input,
+            final String substring,
+            final boolean expected
+    ) {
+        final boolean actual = HangulUtils.contains(input, substring);
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/compare/end-consonant.csv", numLinesToSkip = 1)
+    void givenUnicode_whenCheckEnd_returnResult(
+            final char input,
+            final boolean expected
+    ) {
+        final boolean actual = HangulUtils.isEndsWithConsonant(input);
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/compare/text-end-consonant.csv", numLinesToSkip = 1)
+    void givenText_whenCheckEnd_returnResult(
+            final String input,
+            final boolean expected
+    ) {
+        final boolean actual = HangulUtils.isEndsWithConsonant(input);
+        assertThat(actual).isEqualTo(expected);
+    }
+
+}
